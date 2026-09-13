0f6b07baa45ea369921f53590630189fa620d9b3
uiems0 <alexshorty568@gmail.com>
2026-09-13 14:34:46 -0700

    Update Data.rb

--- Data.rb
+++ Data.rb
@@ -1,1 +1,15 @@
-
+NewData = {#I will add more data later, this is just so I can frame how I want it to work. Also I still need to actually come up with the story
+    ["Section1"] => {
+        ["Background"] => "HTMLINKHERE",
+        ["Scene1"] => {
+            ["MC"] => "MAINCHARACTERIMG",#MC will always be seen
+            ["SC1"] => "SIDECHARACTERIMG",
+            ["SC1Name"] => "SideCharacterName",
+            #Extra Side Characters can be added
+            ["TextSayer"] => "PersonWhosTalkingName",
+            ["Text"] = "Self explanatory"
+        },
+        ["Question1"] = "Also, self explanatory",
+        ["Question2"] = "Also, self explanatory"
+    }
+}
